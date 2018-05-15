8d7fa9c84a101509ce3dcd8a5a1c326fec40c11c
Joshua Hully <jdh220388@gmail.com>
2018-05-15 15:50:26 +0200

    Added a testclass

---
+++ src/main/java/nl/hva/web/workshops/flashcard/model/Test.java
@@ -0,0 +1,18 @@
+package nl.hva.web.workshops.flashcard.model;
+
+public class Test {
+
+    private String name;
+
+    public Test(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
